506506d6d4cc9c5c9f7e319087e47615e1438ad8
Kevin Harrington <mad.hephaestus@gmail.com>
2017-01-29 23:53:21 -0500

    formatting

--- examples/Arduino/WiiServoMap/WiiServoMap.ino
+++ examples/Arduino/WiiServoMap/WiiServoMap.ino
@@ -13,30 +13,23 @@ void setup() {
 	//myChuck.type=OFFICIALWII;
 	myChuck.type = THIRDPARTYWII;
 	//myChuck.type = WIICLASSIC;
-	
-	
-	myChuck. addControlMap(D3, // Servo IO pin
+
+	myChuck.addControlMap(D3, // Servo IO pin
 			17, // Servo low value
 			50, // servo middle
 			160, // servo upper bound
 			-63, // ontroller lower bound
 			0, // controller middle
 			100, //controller upper bound
-			JOY_X);// Enum for the data source
-	myChuck. addControlMap(D4,
-			160,// mapping cna be reversed by swapping upper and lower
+			JOY_X); // Enum for the data source
+	myChuck.addControlMap(D4, 160, // mapping cna be reversed by swapping upper and lower
 			50, // swapping upper and lower pivots around center
 			17, // upper value need not be larger than the lower
 			-50, //lower bound cna be non symetric with upper bound
-			0,
-			100,
-			JOY_Y);
-	myChuck.addButtonMap(D6, 17,
-					160,
-					ZBUTTON);
+			0, 100, JOY_Y);
+	myChuck.addButtonMap(D6, 17, 160, ZBUTTON);
 	myChuck.begin();
-	
-	
+
 }
 void loop() {
 
