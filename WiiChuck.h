b1d2fb0443380ab31cbcc77c52e3bd235f1fbfcd
Kevin Harrington <mad.hephaestus@gmail.com>
2017-02-12 01:24:53 -0500

    Alexs changes to allow for classic controller

--- WiiChuck.h
+++ WiiChuck.h
@@ -1,23 +1,5 @@
-/*
- WiiChuck.cpp - Arduino/chipKit library support for the WiiChuck
- Copyright (C)2015 Rinky-Dink Electronics, Henning Karlsen. All right reserved
-
- This library has been made to easily interface and use the WiiChuck with
- an Arduino or chipKit.
-
- You can find the latest version of the library at
- http://www.RinkyDinkElectronics.com/
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the CC BY-NC-SA 3.0 license.
- Please see the included documents for further information.
-
- Commercial use of this library requires you to buy a license that
- will allow commercial use. This includes using the library,
- modified or not, as a tool to sell products.
-
- The license applies to all part of the library including the
- examples and tools supplied with the library.
+/**
+ * @author Kevin Harrington, Alex Camilo
  */
 #ifndef WiiChuck_h
 #define WiiChuck_h
