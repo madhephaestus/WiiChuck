4fd64740f3172e9f2273ce1e8dcf0947ee4a1b72
Kevin Harrington <mad.hephaestus@gmail.com>
2017-02-12 20:50:58 -0500

    cleaned the print

--- src/controllers/Nunchuck.cpp
+++ src/controllers/Nunchuck.cpp
@@ -34,24 +34,9 @@ boolean Nunchuck::checkButtonZ() {
 
 void Nunchuck::printInputs(Stream& stream) {
 	char st[100];
-	stream.print("Accessory Bytes:\t");
-	for (int i = 0; i < 6; i++) {
-		if (_dataarray[i] < 0x10) {
-			stream.print("0");
-		}
-		stream.print(_dataarray[i], HEX);
-		stream.print(" ");
-	}
-
 	stream.println("");
 
 	stream.print("NUNCHUCK ");
-	stream.print("JoyX BYTE: ");
-	//stream.print(_dataarray[0], BIN) ;
-	stream.print(_dataarray[0], HEX);
-	stream.print("  JoyY BYTE: ");
-	//stream.print(_dataarray[1], BIN) ;
-	stream.print(_dataarray[1], HEX);
 	sprintf(st,
 			"  JoyX: %4d%% | JoyY: %4d%% | Roll: %4d | Pitch: %4d | Buttons: ",
 			getJoyX(), getJoyY(), getRollAngle(), getPitchAngle());
