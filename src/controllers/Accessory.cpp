14c2a4d63c4f87eb8d9fab0f2f08aa1c86ecb688
Kevin Harrington <mad.hephaestus@gmail.com>
2017-02-12 19:00:52 -0500

    formatting

--- src/controllers/Accessory.cpp
+++ src/controllers/Accessory.cpp
@@ -1,86 +1,89 @@
 #include "Accessory.h"
 #include <Wire.h>
 
-
 Accessory::Accessory(uint8_t data_pin, uint8_t sclk_pin) {
 	_sda_pin = data_pin;
 	_scl_pin = sclk_pin;
 	_clockSpacing = 1;
 	ackTimeout = 100;
-	usePullUpClock=false;
+	usePullUpClock = false;
 
 }
 
-uint8_t* Accessory::getDataArray(){
-  return _dataarray;
+uint8_t* Accessory::getDataArray() {
+	return _dataarray;
 }
 
-void Accessory::setDataArray(uint8_t data[6]){
-  for(int i=0; i<6; i++) _dataarray[i] = data[i];
-}
-  
-void Accessory::printInputs(Stream& stream){
-  stream.print("Accessory Bytes:\t");
-  for(int i=0; i<6; i++) {
-         if (_dataarray[i]<0x10) {stream.print("0");} 
-         stream.print(_dataarray[i],HEX); 
-         stream.print(" "); 
-  }
-  stream.println("");
+void Accessory::setDataArray(uint8_t data[6]) {
+	for (int i = 0; i < 6; i++)
+		_dataarray[i] = data[i];
 }
 
-int   Accessory::decodeInt(uint8_t msbbyte,uint8_t msbstart,uint8_t msbend,
-                           uint8_t csbbyte, uint8_t csbstart, uint8_t csbend,
-                           uint8_t lsbbyte, uint8_t lsbstart, uint8_t lsbend){
+void Accessory::printInputs(Stream& stream) {
+	stream.print("Accessory Bytes:\t");
+	for (int i = 0; i < 6; i++) {
+		if (_dataarray[i] < 0x10) {
+			stream.print("0");
+		}
+		stream.print(_dataarray[i], HEX);
+		stream.print(" ");
+	}
+	stream.println("");
+}
+
+int Accessory::decodeInt(uint8_t msbbyte, uint8_t msbstart, uint8_t msbend,
+		uint8_t csbbyte, uint8_t csbstart, uint8_t csbend, uint8_t lsbbyte,
+		uint8_t lsbstart, uint8_t lsbend) {
 // 5 bit int split across 3 bytes. what... the... fuck... nintendo...
 
-  if (msbbyte>5) return false;
-  if (csbbyte>5) return false;
-  if (lsbbyte>5) return false;
+	if (msbbyte > 5)
+		return false;
+	if (csbbyte > 5)
+		return false;
+	if (lsbbyte > 5)
+		return false;
 
-  uint32_t analog=0;
-  uint32_t lpart;
-  lpart = _dataarray[lsbbyte];
-  lpart = lpart >> lsbstart;
-  lpart = lpart &  (0xFF>>(7-(lsbend-lsbstart)) ) ;
-  
-  uint32_t cpart;
-  cpart = _dataarray[csbbyte];
-  cpart = cpart >> csbstart;
-  cpart = cpart &  (0xFF>>(7-(csbend-csbstart)) ) ;
-  
-  cpart = cpart << ((lsbend-lsbstart)+1);
-  
-  uint32_t mpart;
-  mpart = _dataarray[msbbyte];
-  mpart = mpart >> msbstart;
-  mpart = mpart &  (0xFF>>(7-(msbend-msbstart)) ) ;
-  
-  mpart = mpart << ( ((lsbend-lsbstart)+1) + ((csbend-csbstart)+1) ) ;
-  
-  analog = lpart | cpart | mpart;
-  
-  return analog;
-  
+	uint32_t analog = 0;
+	uint32_t lpart;
+	lpart = _dataarray[lsbbyte];
+	lpart = lpart >> lsbstart;
+	lpart = lpart & (0xFF >> (7 - (lsbend - lsbstart)));
+
+	uint32_t cpart;
+	cpart = _dataarray[csbbyte];
+	cpart = cpart >> csbstart;
+	cpart = cpart & (0xFF >> (7 - (csbend - csbstart)));
+
+	cpart = cpart << ((lsbend - lsbstart) + 1);
+
+	uint32_t mpart;
+	mpart = _dataarray[msbbyte];
+	mpart = mpart >> msbstart;
+	mpart = mpart & (0xFF >> (7 - (msbend - msbstart)));
+
+	mpart = mpart << (((lsbend - lsbstart) + 1) + ((csbend - csbstart) + 1));
+
+	analog = lpart | cpart | mpart;
+
+	return analog;
+
 }
 
-bool  Accessory::decodeBit(uint8_t byte, uint8_t bit, bool activeLow){
-  if (byte>5) return false;
-  uint8_t swb = _dataarray[byte];
-  uint8_t sw   = (swb>>bit) & 0x01;
-  return activeLow?(!sw):(sw);
-} 
+bool Accessory::decodeBit(uint8_t byte, uint8_t bit, bool activeLow) {
+	if (byte > 5)
+		return false;
+	uint8_t swb = _dataarray[byte];
+	uint8_t sw = (swb >> bit) & 0x01;
+	return activeLow ? (!sw) : (sw);
+}
 
-void Accessory::begin()
-{
-
+void Accessory::begin() {
 
 	_use_hw = false;
-	if (	(_sda_pin == SDA) and (_scl_pin == SCL))
-	{
+	if ((_sda_pin == SDA) and (_scl_pin == SCL)) {
 		_use_hw = true;
-		  Wire.begin();
-		  Serial.println("Starting Wire");
+		Wire.begin();
+		Serial.println("Starting Wire");
 
 	}
 	_initBytes();
@@ -95,7 +98,6 @@ void Accessory::begin()
 
 }
 
-
 /*
  * public function to read data
  */
@@ -107,62 +109,61 @@ void Accessory::readData() {
 /**
  * Reads the device type from the controller
  */
-ControllerType Accessory::identifyController(){
+ControllerType Accessory::identifyController() {
 	_burstReadWithAddress(0xfe);
 
-  if (_dataarray[0] == 0x01)
-    if (_dataarray[1] == 0x01)
-      return WIICLASSIC; // Classic Controller
+	if (_dataarray[0] == 0x01)
+		if (_dataarray[1] == 0x01)
+			return WIICLASSIC; // Classic Controller
 
-  if (_dataarray[0] == 0x00)
-    if (_dataarray[1] == 0x00)
-      return NUNCHUCK; // nunchuck
+	if (_dataarray[0] == 0x00)
+		if (_dataarray[1] == 0x00)
+			return NUNCHUCK; // nunchuck
 
-  // It's something else.
-  _burstReadWithAddress(0xfa);
+	// It's something else.
+	_burstReadWithAddress(0xfa);
 
-  if (_dataarray[0] == 0x00)
-    if (_dataarray[1] == 0x00)
-      if (_dataarray[2] == 0xa4)
-        if (_dataarray[3] == 0x20)
-          if (_dataarray[4] == 0x01)
-            if (_dataarray[5] == 0x03)
-              return GuitarHeroController; // Guitar Hero Controller
+	if (_dataarray[0] == 0x00)
+		if (_dataarray[1] == 0x00)
+			if (_dataarray[2] == 0xa4)
+				if (_dataarray[3] == 0x20)
+					if (_dataarray[4] == 0x01)
+						if (_dataarray[5] == 0x03)
+							return GuitarHeroController; // Guitar Hero Controller
 
-  if (_dataarray[0] == 0x01)
-    if (_dataarray[1] == 0x00)
-      if (_dataarray[2] == 0xa4)
-        if (_dataarray[3] == 0x20)
-          if (_dataarray[4] == 0x01)
-            if (_dataarray[5] == 0x03)
-              return GuitarHeroWorldTourDrums; // Guitar Hero World Tour Drums
+	if (_dataarray[0] == 0x01)
+		if (_dataarray[1] == 0x00)
+			if (_dataarray[2] == 0xa4)
+				if (_dataarray[3] == 0x20)
+					if (_dataarray[4] == 0x01)
+						if (_dataarray[5] == 0x03)
+							return GuitarHeroWorldTourDrums; // Guitar Hero World Tour Drums
 
+	if (_dataarray[0] == 0x03)
+		if (_dataarray[1] == 0x00)
+			if (_dataarray[2] == 0xa4)
+				if (_dataarray[3] == 0x20)
+					if (_dataarray[4] == 0x01)
+						if (_dataarray[5] == 0x03)
+							return GuitarHeroWorldTourDrums; // Guitar Hero World Tour Drums
 
-  if (_dataarray[0] == 0x03)
-    if (_dataarray[1] == 0x00)
-      if (_dataarray[2] == 0xa4)
-        if (_dataarray[3] == 0x20)
-          if (_dataarray[4] == 0x01)
-            if (_dataarray[5] == 0x03)
-              return GuitarHeroWorldTourDrums; // Guitar Hero World Tour Drums
+	if (_dataarray[0] == 0x00)
+		if (_dataarray[1] == 0x00)
+			if (_dataarray[2] == 0xa4)
+				if (_dataarray[3] == 0x20)
+					if (_dataarray[4] == 0x01)
+						if (_dataarray[5] == 0x11)
+							return DrumController; // Taiko no Tatsujin TaTaCon (Drum controller)
 
-  if (_dataarray[0] == 0x00)
-    if (_dataarray[1] == 0x00)
-      if (_dataarray[2] == 0xa4)
-        if (_dataarray[3] == 0x20)
-          if (_dataarray[4] == 0x01)
-            if (_dataarray[5] == 0x11)
-              return DrumController; // Taiko no Tatsujin TaTaCon (Drum controller)
+	if (_dataarray[0] == 0xFF)
+		if (_dataarray[1] == 0x00)
+			if (_dataarray[2] == 0xa4)
+				if (_dataarray[3] == 0x20)
+					if (_dataarray[4] == 0x00)
+						if (_dataarray[5] == 0x13)
+							return DrawsomeTablet; // Drawsome Tablet
 
-  if (_dataarray[0] == 0xFF)
-    if (_dataarray[1] == 0x00)
-      if (_dataarray[2] == 0xa4)
-        if (_dataarray[3] == 0x20)
-          if (_dataarray[4] == 0x00)
-            if (_dataarray[5] == 0x13)
-              return DrawsomeTablet; // Drawsome Tablet
-
-  return Mystery;
+	return Mystery;
 }
 
 /**Private functions*/
@@ -172,7 +173,7 @@ void Accessory::_sendStart(byte addr) {
 	_clockHigh();
 	_dataLow();
 	_clockLow();
-	_shiftOut( addr);
+	_shiftOut(addr);
 
 }
 
@@ -199,44 +200,46 @@ void Accessory::_sendAck() {
 	pinMode(_sda_pin, INPUT);
 }
 
-void Accessory::_dataHigh(){
+void Accessory::_dataHigh() {
 	//Serial.println("high");
-	if(usePullUpClock){
+	if (usePullUpClock) {
 		pinMode(_sda_pin, INPUT);
-	}else{
+	} else {
 		pinMode(_sda_pin, OUTPUT);
 		digitalWrite(_sda_pin, HIGH);
 	}
 
 }
-void Accessory::_dataLow(){
+void Accessory::_dataLow() {
 	//Serial.println("low");
 	pinMode(_sda_pin, OUTPUT);
 	digitalWrite(_sda_pin, LOW);
 
 }
-void Accessory::_clockHigh(){
+void Accessory::_clockHigh() {
 	//Serial.println("high");
-	if(usePullUpClock){
+	if (usePullUpClock) {
 		_clockStallCheck();
-	}else{
+	} else {
 		pinMode(_scl_pin, OUTPUT);
 		digitalWrite(_scl_pin, HIGH);
 	}
 //	pinMode(_scl_pin, OUTPUT);
 //	digitalWrite(_scl_pin, HIGH);
-	if (_clockSpacing > 0)delayMicroseconds(_clockSpacing);
+	if (_clockSpacing > 0)
+		delayMicroseconds(_clockSpacing);
 
 }
-void Accessory::_clockLow(){
+void Accessory::_clockLow() {
 	//Serial.println("low");
 	pinMode(_scl_pin, OUTPUT);
 	digitalWrite(_scl_pin, LOW);
-	if (_clockSpacing > 0)delayMicroseconds(_clockSpacing);
+	if (_clockSpacing > 0)
+		delayMicroseconds(_clockSpacing);
 
 }
 
-void Accessory::_clockStallCheck(){
+void Accessory::_clockStallCheck() {
 	pinMode(_scl_pin, INPUT);
 
 	unsigned long time = millis();
@@ -269,61 +272,57 @@ uint8_t Accessory::_readByte() {
 }
 
 void Accessory::_writeByte(uint8_t value) {
-	_shiftOut( value);
+	_shiftOut(value);
 }
 
-
-void Accessory::_shiftOut( uint8_t val) {
+void Accessory::_shiftOut(uint8_t val) {
 	uint8_t i;
 	for (i = 0; i < 8; i++) {
-		if((val & (1 << (7 - i))) == 0) {
-			_dataLow() ;
-		}else
+		if ((val & (1 << (7 - i))) == 0) {
+			_dataLow();
+		} else
 			_dataHigh();
 		_clockHigh();
 		_clockLow();
 	}
 }
 
-
-void Accessory::_burstRead(){
+void Accessory::_burstRead() {
 	_burstReadWithAddress(0);
 }
 
-void Accessory::_burstReadWithAddress(uint8_t addr)
-{
-	int readAmnt =6;
+void Accessory::_burstReadWithAddress(uint8_t addr) {
+	int readAmnt = 6;
 
-	if (_use_hw)
-	{
-		 // send conversion command
-		  Wire.beginTransmission(I2C_ADDR);
-		  Wire.write(addr);
-		  Wire.endTransmission();
+	if (_use_hw) {
+		// send conversion command
+		Wire.beginTransmission(I2C_ADDR);
+		Wire.write(addr);
+		Wire.endTransmission();
 
-		  // wait for data to be converted
-		  delay(1);
+		// wait for data to be converted
+		delay(1);
 
-		  // read data
-		  Wire.readBytes(_dataarray, Wire.requestFrom(I2C_ADDR, sizeof(_dataarray)));
+		// read data
+		Wire.readBytes(_dataarray,
+				Wire.requestFrom(I2C_ADDR, sizeof(_dataarray)));
 
-	}else{
-	// send conversion command
+	} else {
+		// send conversion command
 		_sendStart(I2C_ADDR_W);
 		_waitForAck();
 		_writeByte(addr);
 		_waitForAck();
 		_sendStop();
 		// wait for data to be converted
-		  delay(1);
+		delay(1);
 		_sendStart(I2C_ADDR_R);
 		_waitForAck();
 
-		for (int i=0; i<readAmnt; i++)
-		{
+		for (int i = 0; i < readAmnt; i++) {
 			delayMicroseconds(40);
-			_dataarray[i] = _readByte()  ;
-			if (i<(readAmnt-1))
+			_dataarray[i] = _readByte();
+			if (i < (readAmnt - 1))
 				_sendAck();
 			else
 				_sendNack();
@@ -332,18 +331,16 @@ void Accessory::_burstReadWithAddress(uint8_t addr)
 	}
 }
 
-void Accessory::_writeRegister(uint8_t reg, uint8_t value)
-{
-	if (_use_hw)
-	{
+void Accessory::_writeRegister(uint8_t reg, uint8_t value) {
+	if (_use_hw) {
 		//Serial.println("Writing reg");
-		  Wire.beginTransmission(I2C_ADDR);
-		  Wire.write(reg);
-		  Wire.write(value);
-		  Wire.endTransmission();
-		  //Serial.println("Writing reg done");
-	}else{
-	//Serial.println("Sending start");
+		Wire.beginTransmission(I2C_ADDR);
+		Wire.write(reg);
+		Wire.write(value);
+		Wire.endTransmission();
+		//Serial.println("Writing reg done");
+	} else {
+		//Serial.println("Sending start");
 		_sendStart(I2C_ADDR_W);
 		_waitForAck();
 		//Serial.println("First byte");
@@ -360,9 +357,9 @@ void Accessory::_writeRegister(uint8_t reg, uint8_t value)
 }
 void Accessory::_initBytes() {
 
-		// improved startup procedure from http://playground.arduino.cc/Main/AccessoryClass
-		_writeRegister(0xF0, 0x55);
-		_writeRegister(0xFB, 0x00);
+	// improved startup procedure from http://playground.arduino.cc/Main/AccessoryClass
+	_writeRegister(0xF0, 0x55);
+	_writeRegister(0xFB, 0x00);
 
 }
 
--- src/controllers/Accessory.h
+++ src/controllers/Accessory.h
@@ -11,102 +11,107 @@
 //#define THIRDPARTYWII 0
 //#define OFFICIALWII 1
 //#define WIICLASSIC 2
-typedef enum _controllertype {NUNCHUCK,WIICLASSIC,GuitarHeroController,GuitarHeroWorldTourDrums,DrumController,DrawsomeTablet,Mystery} ControllerType;
-
+typedef enum _controllertype {
+	NUNCHUCK,
+	WIICLASSIC,
+	GuitarHeroController,
+	GuitarHeroWorldTourDrums,
+	DrumController,
+	DrawsomeTablet,
+	Mystery
+} ControllerType;
 
 class Accessory {
-  public:
-    Accessory(uint8_t data_pin, uint8_t sclk_pin);
-    
-    uint8_t* getDataArray();
-    void setDataArray(uint8_t data[6]);
-  
-    void printInputs(Stream& stream);
-    
+public:
+	Accessory(uint8_t data_pin, uint8_t sclk_pin);
 
-	  void begin();
-	  void readData();
+	uint8_t* getDataArray();
+	void setDataArray(uint8_t data[6]);
 
-	  boolean usePullUpClock;
-	  ControllerType identifyController();
-  private:
+	void printInputs(Stream& stream);
 
-    // Data Parsing
-    int   decodeInt(uint8_t msbbyte,uint8_t msbstart,uint8_t msbend,
-                           uint8_t csbbyte, uint8_t csbstart, uint8_t csbend,
-                           uint8_t lsbbyte, uint8_t lsbstart, uint8_t lsbend);
-    bool  decodeBit(uint8_t byte, uint8_t bit, bool activeLow); 
-    
-	  // SW i2c
-	  uint8_t _scl_pin;
-	  uint8_t _sda_pin;
-	  boolean _use_hw;
-	  uint32_t _clockSpacing;
-    unsigned long ackTimeout;
-	  
-	  void _clockHigh();
-	  void _clockLow();
-	  void _dataHigh();
-	  void _dataLow();
-	  void _sendStart(byte addr);
-	  void _sendStop();
-	  void _sendAck();
-	  void _sendNack();
-	  void _waitForAck();
-	  void _clockStallCheck();
-	  void _shiftOut(uint8_t val);
-	  uint8_t _readByte();
-	  void _writeByte(uint8_t value);
-	  
-	  // Controller Register Transactions
-	  uint8_t _dataarray[8];
+	void begin();
+	void readData();
 
-	  void _burstRead();
-	  void _burstReadWithAddress(uint8_t addr);
-	  void _writeRegister(uint8_t reg, uint8_t value);
-	  void _initBytes();
+	boolean usePullUpClock;
+	ControllerType identifyController();
+private:
 
+	// Data Parsing
+	int decodeInt(uint8_t msbbyte, uint8_t msbstart, uint8_t msbend,
+			uint8_t csbbyte, uint8_t csbstart, uint8_t csbend, uint8_t lsbbyte,
+			uint8_t lsbstart, uint8_t lsbend);
+	bool decodeBit(uint8_t byte, uint8_t bit, bool activeLow);
 
+	// SW i2c
+	uint8_t _scl_pin;
+	uint8_t _sda_pin;
+	boolean _use_hw;
+	uint32_t _clockSpacing;
+	unsigned long ackTimeout;
 
+	void _clockHigh();
+	void _clockLow();
+	void _dataHigh();
+	void _dataLow();
+	void _sendStart(byte addr);
+	void _sendStop();
+	void _sendAck();
+	void _sendNack();
+	void _waitForAck();
+	void _clockStallCheck();
+	void _shiftOut(uint8_t val);
+	uint8_t _readByte();
+	void _writeByte(uint8_t value);
+
+	// Controller Register Transactions
+	uint8_t _dataarray[8];
+
+	void _burstRead();
+	void _burstReadWithAddress(uint8_t addr);
+	void _writeRegister(uint8_t reg, uint8_t value);
+	void _initBytes();
 
 };
 
-typedef enum _inType {ANALOG,DIGITAL} inType;
+typedef enum _inType {
+	ANALOG, DIGITAL
+} inType;
 
 typedef struct _inputMapping {
-  inType  type;
-  
-  // digital in params
-  uint8_t dByte;
-  uint8_t dBit;
-  bool    dActiveLow;
-  
-  // analog in params
-  uint8_t aMsbbyte;
-  uint8_t aMsbstart;
-  uint8_t aMsbend;
-  
-  uint8_t aCsbbyte;
-  uint8_t aCsbstart;
-  uint8_t aCsbend;
-  
-  uint8_t aLsbbyte;
-  uint8_t aLsbstart;
-  uint8_t aLsbend;
-  
-  // Analog Scaling info
-  uint8_t aMax;
-  uint8_t aZero;
-  uint8_t aMin;
-  
-  // Servo Scaling Info
-  uint8_t sevoMax;
-  uint8_t servoZero;
-  uint8_t servoMin;
-  
-  Servo   servo;
-  
-  struct _inputMapping* nextMap;
+	inType type;
+
+	// digital in params
+	uint8_t dByte;
+	uint8_t dBit;
+	bool dActiveLow;
+
+	// analog in params
+	uint8_t aMsbbyte;
+	uint8_t aMsbstart;
+	uint8_t aMsbend;
+
+	uint8_t aCsbbyte;
+	uint8_t aCsbstart;
+	uint8_t aCsbend;
+
+	uint8_t aLsbbyte;
+	uint8_t aLsbstart;
+	uint8_t aLsbend;
+
+	// Analog Scaling info
+	uint8_t aMax;
+	uint8_t aZero;
+	uint8_t aMin;
+
+	// Servo Scaling Info
+	uint8_t sevoMax;
+	uint8_t servoZero;
+	uint8_t servoMin;
+
+	Servo servo;
+
+	struct _inputMapping* nextMap;
 } inputMapping;
 
 #endif
