89c7e0593c5a3dd4474de75b96eefa904b4c92de
Kevin Harrington <mad.hephaestus@gmail.com>
2017-02-12 23:08:12 -0500

    Making the decode functions protected

--- src/controllers/Accessory.h
+++ src/controllers/Accessory.h
@@ -131,13 +131,14 @@ public:
 protected:
 	// allow sub classes to view the data
 	uint8_t _dataarray[8];
-private:
-
 	// Data Parsing
 	int decodeInt(uint8_t msbbyte, uint8_t msbstart, uint8_t msbend,
 			uint8_t csbbyte, uint8_t csbstart, uint8_t csbend, uint8_t lsbbyte,
 			uint8_t lsbstart, uint8_t lsbend);
 	bool decodeBit(uint8_t byte, uint8_t bit, bool activeLow);
+private:
+
+
 
 	// SW i2c
 	uint8_t _scl_pin;
