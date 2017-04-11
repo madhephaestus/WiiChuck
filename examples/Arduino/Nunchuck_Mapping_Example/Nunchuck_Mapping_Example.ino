1a4bc4f0b394cab51ab8bccfb6ff2143eb75aa59
Kevin Harrington <mad.hephaestus@gmail.com>
2017-04-11 13:27:31 -0400

    Update Nunchuck_Mapping_Example.ino

--- examples/Arduino/Nunchuck_Mapping_Example/Nunchuck_Mapping_Example.ino
+++ examples/Arduino/Nunchuck_Mapping_Example/Nunchuck_Mapping_Example.ino
@@ -2,10 +2,6 @@
 #include <WiiChuck.h>
 #include <Wire.h>
 
-#if defined(ARDUINO_ARCH_ESP8266)
-#define SDA D2
-#define SCL D1
-#endif
 // This works with the Guitar Hero World Tour (Wii) Drums
 
 Nunchuck nunchuck(SDA, SCL);
@@ -17,18 +13,20 @@ void setup() {
 
   nunchuck.begin();
 
-  nunchuck.addMap(new Nunchuck::joyX(D5,200,128,10)); // Servo max zero min cooldown
-                                                           // cooldown: hold value for x ms.
+  nunchuck.addMap(new Nunchuck::joyX(5,200,128,10)); // Servo pin, max servo value, zero center value, min servo value 
                                                            
-  nunchuck.addMap(new Nunchuck::joyY(D6,200,128,10)); // Servo max zero min cooldown
-                                                           // cooldown: hold value for x ms.                                                           
+  nunchuck.addMap(new Nunchuck::joyY(6,200,128,10)); // Servo pin, max servo value, zero center value, min servo value 
   nunchuck.printMaps(Serial);  
 }
 
 
 void loop() {
-	nunchuck.readData();   // Read inputs and update maps
-	nunchuck.printInputs(Serial); // Print all inputs
-  
-	delay(50);
+  nunchuck.readData();   // Read inputs and update maps
+  nunchuck.printInputs(Serial); // Print all inputs
+  Serial.print(" Raw value x: ");
+  Serial.print(nunchuck.getJoyX());
+  Serial.print(" y: ");
+  Serial.print(nunchuck.getJoyY());
+  Serial.print(" ");
+  delay(50);
 }
